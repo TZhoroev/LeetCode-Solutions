3dc6488a56355fd3406eabb97b67afff5fea4ab0
TZhoroev <105243578+TZhoroev@users.noreply.github.com>
2022-10-18 22:15:04 -0400

    Time: 115 ms (35.74%), Space: 14.3 MB (17.96%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -3,7 +3,7 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         d1 = Counter(ransomNote)
         d2 = Counter(magazine)
-        for item in ransomNote:
+        for item in d1:
             if d1[item]>d2[item]:return False
         return True
         
