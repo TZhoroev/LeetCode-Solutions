b569a87f0d279066c9d0c44c0191ea0e29107a9f
Tilekbek Zhoroev <tilekbekjoroev@gmail.com>
2022-11-26 19:10:58 -0500

    Time: 5575 ms (14.94%), Space: 154.9 MB (75.50%) - LeetHub

---
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+class Solution:
+    def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
+        graph = [[] for _ in range(len(roads) + 1)]
+        for u, v in roads:
+            graph[u].append(v)
+            graph[v].append(u)
+        def dfs(v, par):
+            cost, people = 0, 1
+            for u in graph[v]:
+                if u != par:
+                    c, p = dfs(u, v)
+                    cost += c + ceil(p / seats)
+                    people += p
+            return cost, people
+        ans, _ = dfs(0, -1)
+        return ans
+                
+                
+            
