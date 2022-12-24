939f3929a1d08742a0e4ab8f24449904a6fea595
Tilekbek Zhoroev <tilekbekjoroev@gmail.com>
2022-12-23 19:24:11 -0500

    Time: 33 ms (95.87%), Space: 13.9 MB (85.40%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -1,9 +1,9 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        @cache
-        def dp(n):
-            if n ==1: return 1
-            elif n==2: return 2
-            elif n==3: return 5
-            else: return (2*dp(n-1)+dp(n-3)) % (10 ** 9 + 7)
-        return dp(n)
+        if n ==1: return 1
+        elif n==2: return 2
+        elif n==3: return 5
+        a, b, c = 5, 2, 1
+        for i in range(4, n + 1):
+            a, b, c = 2 * a + c, a, b
+        return a % (10 ** 9 + 7)
