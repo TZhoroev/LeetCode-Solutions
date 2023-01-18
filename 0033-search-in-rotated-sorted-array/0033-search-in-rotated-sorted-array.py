6c60268a11741c6ae894349d7cebb9b89b292191
Tilekbek Zhoroev <tilekbekjoroev@gmail.com>
2023-01-18 15:32:07 -0500

    Time: 51 ms (47.57%), Space: 14.3 MB (51.52%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -6,12 +6,12 @@ class Solution:
             mid = (left + right) // 2
             if nums[mid] == target: return mid
             if nums[right] > nums[mid]: 
-                if nums[right] >= target > nums[mid]: 
+                if nums[right] >= target and target > nums[mid]: 
                     left = mid + 1
                 else: right = mid - 1
-            else: # if it's rotated form
-                if nums[left]<= target < nums[mid]: # if the target is in left half
+            else:
+                if nums[left]<= target and target < nums[mid]: 
                     right = mid - 1
-                else: left = mid + 1 # if the target is in right half
+                else: left = mid + 1 
         return -1
         
