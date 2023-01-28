2b0490b9d3811f569c479ac07c6e87c71ec73b96
Tilekbek Zhoroev <tilekbekjoroev@gmail.com>
2023-01-28 13:02:13 -0500

    Time: 1477 ms (66.52%), Space: 27.5 MB (20.98%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        left, right = 0, max(candies)
+        def helper(c_max):
+            if c_max == 0:
+                return True
+            c_candies = 0
+            for candie in candies:
+                c_candies += candie//c_max
+            return c_candies >= k
+        ans = 0
+        while left <= right:
+            mid = (left + right)//2
+            if helper(mid):
+                left = mid + 1
+                ans = mid
+                print(mid)
+            else:
+                right = mid - 1
+        return ans 
+        
