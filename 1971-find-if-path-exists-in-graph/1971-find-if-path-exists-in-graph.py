168cda1995fa41a91e23bacfebce93adf82f9f25
Tilekbek Zhoroev <tilekbekjoroev@gmail.com>
2023-01-30 15:23:02 -0500

    Time: 1988 ms (77.05%), Space: 107.2 MB (39.46%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -5,8 +5,21 @@ class Solution:
         for u, v in edges:
             self.adj[u].append(v)
             self.adj[v].append(u)
-        return self.dfs(source, destination)
+        return self.bfs(source, destination)
     
+    def bfs(self, node, target):
+        Q = deque()
+        Q.append(node)
+        self.visited[node] = True
+        while Q:
+            node = Q.popleft()
+            if node == target: return True
+            for v in self.adj[node]:
+                if not self.visited[v]:
+                    self.visited[v] = True
+                    Q.append(v)
+        return False
+
     def dfs(self, node, target):
         if node == target: return True
         self.visited[node] = True
